d9d3c937b669cbe26f6fb3da1d50afb5220e4544
Manan Chataut <mananchataut@gmail.com>
2026-06-11 23:37:56 +0530

    feat: add go.mod and main.go entry point

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"math/rand"
+	"time"
+
+	"github.com/heyits-manan/dclone/cmd"
+)
 
 func main() {
-	fmt.Println("dclone - a simple container runtime")
+	rand.Seed(time.Now().UnixNano())
+
+	// Check if we're the child process (re-execed by parent)
+	if len(os.Args) > 1 && os.Args[1] == "child" {
+		// Extract args: child <rootfs> <command> [args...]
+		rootfs := os.Args[2]
+		command := os.Args[3]
+		commandArgs := os.Args[4:]
+
+		// Run child logic directly, bypass Cobra
+		if err := runtime.RunChild(rootfs, command, commandArgs); err != nil {
+			panic(err)
+		}
+		return
+	}
+
+	// Normal CLI path
+	cmd.Execute()
 }
